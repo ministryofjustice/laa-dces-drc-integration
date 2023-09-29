42eb6c63bc7ea060799fa55b58528fe806418b49
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-09-29 11:15:00 +0100

    Initial commit

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/DcesDrcIntegrationApplication.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/DcesDrcIntegrationApplication.java
@@ -1,13 +1,13 @@
-package uk.gov.justice.laa.crime.microservice;
+package uk.gov.justice.laa.crime.dces.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceTemplateApplication {
+public class DcesDrcIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceTemplateApplication.class, args);
+		SpringApplication.run(DcesDrcIntegrationApplication.class, args);
 	}
 
 }
