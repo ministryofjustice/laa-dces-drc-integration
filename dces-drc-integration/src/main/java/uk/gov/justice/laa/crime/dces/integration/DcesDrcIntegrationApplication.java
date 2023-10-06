dd1eb31b3c0b6abbe1e41d9e3906d971580fa985
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-10-06 14:36:01 +0100

    Adding micrometer and basic things needed by gradle.

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/DcesDrcIntegrationApplication.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/DcesDrcIntegrationApplication.java
@@ -2,8 +2,10 @@ package uk.gov.justice.laa.crime.dces.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DcesDrcIntegrationApplication {
 
 	public static void main(String[] args) {
