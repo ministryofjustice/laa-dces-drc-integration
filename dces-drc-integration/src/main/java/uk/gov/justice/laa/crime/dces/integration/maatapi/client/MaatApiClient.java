d16f6dadb1fff3cc9e68af0433a7e85214fff49f
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2023-12-14 17:03:49 +0000

    Fixing interface.

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/maatapi/client/MaatApiClient.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/maatapi/client/MaatApiClient.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.crime.dces.integration.maatapi.client;
 
 import org.springframework.web.service.annotation.GetExchange;
-import uk.gov.justice.laa.crime.dces.integration.model.ContributionGetResponse;
 
 
 public interface MaatApiClient {
