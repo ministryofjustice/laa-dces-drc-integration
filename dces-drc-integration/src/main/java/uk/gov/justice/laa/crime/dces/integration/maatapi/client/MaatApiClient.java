9fd9a66f9f8b1825ac3dc9ba037523db1bd44683
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2024-01-09 10:05:34 +0000

    Removing un-needed interface method signature.

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/maatapi/client/MaatApiClient.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/maatapi/client/MaatApiClient.java
@@ -1,10 +1,5 @@
 package uk.gov.justice.laa.crime.dces.integration.maatapi.client;
 
-import org.springframework.web.service.annotation.GetExchange;
-
-
 public interface MaatApiClient {
 
-    @GetExchange("/get")
-    Boolean sendRequest();
 }
