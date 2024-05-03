754d7884ea2ade76c31cc2c4f2776794e98d63c0
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2024-05-03 14:00:17 +0100

    Adding initial proving tests.

--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/ContributionIntegrationTest.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/ContributionIntegrationTest.java
@@ -1,143 +1,34 @@
 package uk.gov.justice.laa.crime.dces.integration.service;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-import jakarta.xml.bind.JAXBException;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
-import org.springframework.web.client.HttpServerErrorException;
-import uk.gov.justice.laa.crime.dces.integration.client.DrcClient;
 import uk.gov.justice.laa.crime.dces.integration.model.drc.UpdateLogContributionRequest;
-import uk.gov.justice.laa.crime.dces.integration.model.generated.contributions.CONTRIBUTIONS;
-import uk.gov.justice.laa.crime.dces.integration.model.generated.contributions.ObjectFactory;
-import uk.gov.justice.laa.crime.dces.integration.utils.ContributionsMapperUtils;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.*;
 
 @EnabledIf(expression = "#{environment['sentry.environment'] == 'DEV'}", loadContext = true)
 @SpringBootTest
 @ExtendWith(SoftAssertionsExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@WireMockTest(httpPort = 1111)
 class ContributionIntegrationTest {
 
 	@InjectSoftAssertions
 	private SoftAssertions softly;
 
-	@MockBean
-	ContributionsMapperUtils contributionsMapperUtilsMock;
-
-	@MockBean
-	private DrcClient drcClient;
 
 	@Autowired
 	private ContributionService contributionService;
 
-	private static final List<StubMapping> customStubs = new ArrayList<>();
-
-	private static final String GET_URL = "/debt-collection-enforcement/concor-contribution-files?status=ACTIVE";
-	private static final String UPDATE_URL = "/debt-collection-enforcement/create-contribution-file";
-
 	@AfterEach
-	void afterTestAssertAll(){
+	void assertAll(){
 		softly.assertAll();
-		for(StubMapping stub: customStubs ){
-			WireMock.removeStub(stub);
-			WireMock.resetAllRequests();
-		}
-	}
-	@Test
-	void testXMLValid() throws JAXBException {
-
-		when(contributionsMapperUtilsMock.mapLineXMLToObject(any())).thenReturn(createTestContribution());
-		when(contributionsMapperUtilsMock.generateFileXML(any(), any())).thenReturn("ValidXML");
-		when(contributionsMapperUtilsMock.generateFileName(any())).thenReturn("TestFilename.xml");
-		when(contributionsMapperUtilsMock.generateAckXML(any(), any(), any(), any())).thenReturn("ValidAckXML");
-		when(drcClient.sendUpdate(any())).thenReturn(true);
-
-		boolean result = contributionService.processDailyFiles();
-		verify(contributionsMapperUtilsMock,times(2)).mapLineXMLToObject(any());
-		verify(contributionsMapperUtilsMock).generateFileXML(any(), any());
-		softly.assertThat(result).isTrue();
-	}
-
-	@Test
-	void testFileXMLInvalid() throws JAXBException {
-		when(contributionsMapperUtilsMock.generateFileXML(any(), any())).thenReturn("InvalidXML");
-		when(contributionsMapperUtilsMock.generateAckXML(any(),any(),any(),any())).thenReturn("AckXML");
-		when(contributionsMapperUtilsMock.generateFileName(any())).thenReturn("FileName");
-		when(drcClient.sendUpdate(any())).thenReturn(true);
-
-		Exception exception = assertThrows(HttpServerErrorException.class, () -> {
-			contributionService.processDailyFiles();
-		});
-		verify(contributionsMapperUtilsMock,times(2)).mapLineXMLToObject(any());
-		// failure to generate the xml should return a null xmlString.
-		verify(contributionsMapperUtilsMock).generateFileXML(any(), any());
-		// failure should be the result of file generation
-
-		String expectedMessage = "500 Received error 500 INTERNAL_SERVER_ERROR due to Internal Server Error";
-		String actualMessage = exception.getMessage();
-
-		softly.assertThat(actualMessage.contains(expectedMessage)).isTrue();
-	}
-
-	@Test
-	void testLineXMLInvalid() throws JAXBException {
-		when(contributionsMapperUtilsMock.mapLineXMLToObject(any())).thenThrow(JAXBException.class);
-		contributionService.processDailyFiles();
-		verify(contributionsMapperUtilsMock,times(2)).mapLineXMLToObject(any());
-		// with no successful xml, should not run the file generation.
-		verify(contributionsMapperUtilsMock, times(0)).generateFileXML(any(), any());
-	}
-
-	@Test
-	void testAtomicUpdateFailure() throws JAXBException {
-		// setup
-		customStubs.add(stubFor(post(UPDATE_URL).atPriority(1)
-				.willReturn(serverError())));
-
-		when(contributionsMapperUtilsMock.mapLineXMLToObject(any())).thenReturn(createTestContribution());
-		when(contributionsMapperUtilsMock.generateFileXML(any(), any())).thenReturn("ValidXML");
-		when(contributionsMapperUtilsMock.generateFileName(any())).thenReturn("TestFilename.xml");
-		when(contributionsMapperUtilsMock.generateAckXML(any(), any(), any(), any())).thenReturn("ValidAckXML");
-		when(drcClient.sendUpdate(any())).thenReturn(true);
-		// do
-		Exception exception = Assert.assertThrows(HttpServerErrorException.class, () -> {
-			contributionService.processDailyFiles();
-		});
-		// test
-		WireMock.verify(1, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(1, postRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
-
-	@Test
-	void testProcessContributionUpdateWhenReturnedTrue() {
-		UpdateLogContributionRequest dataRequest = UpdateLogContributionRequest.builder()
-				.concorId(911)
-				.build();
-		String response = contributionService.processContributionUpdate(dataRequest);
-		assertEquals("The request has been processed successfully", response);
 	}
 
 	@Test
@@ -148,13 +39,19 @@ class ContributionIntegrationTest {
 				.errorText(errorText)
 				.build();
 		String response = contributionService.processContributionUpdate(dataRequest);
-		assertEquals("The request has failed to process", response);
+		softly.assertThat(response).isEqualTo("The request has failed to process");
 	}
 
-	CONTRIBUTIONS createTestContribution(){
-		ObjectFactory of = new ObjectFactory();
-		CONTRIBUTIONS cont = of.createCONTRIBUTIONS();
-		cont.setId(BigInteger.valueOf(3333));
-		return cont;
+
+	@Test
+	void testProcessContributionUpdateWhenReturnedTrue() {
+		String errorText = "Error Text updated successfully.";
+		UpdateLogContributionRequest dataRequest = UpdateLogContributionRequest.builder()
+				.concorId(47959912)
+				.errorText(errorText)
+				.build();
+		String response = contributionService.processContributionUpdate(dataRequest);
+		softly.assertThat(response).isEqualTo("The request has been processed successfully");
 	}
+
 }
--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/FdcIntegrationTest.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/FdcIntegrationTest.java
@@ -1,8 +1,6 @@
 package uk.gov.justice.laa.crime.dces.integration.service;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
@@ -15,20 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
-import org.springframework.web.client.HttpServerErrorException;
 import uk.gov.justice.laa.crime.dces.integration.model.drc.UpdateLogFdcRequest;
-import uk.gov.justice.laa.crime.dces.integration.model.generated.fdc.ObjectFactory;
 import uk.gov.justice.laa.crime.dces.integration.utils.FdcMapperUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.*;
 
 @EnabledIf(expression = "#{environment['sentry.environment'] == 'DEV'}", loadContext = true)
 @SpringBootTest
@@ -50,135 +39,13 @@ class FdcIntegrationTest {
 	@AfterEach
 	void afterTestAssertAll(){
 		softly.assertAll();
-		for(StubMapping stub: customStubs ){
-			WireMock.removeStub(stub);
-		}
 	}
 
-	private static final List<StubMapping> customStubs = new ArrayList<>();
-	private static final String GET_URL = "/debt-collection-enforcement/fdc-contribution-files?status=REQUESTED";
-	private static final String PREPARE_URL = "/debt-collection-enforcement/prepare-fdc-contributions-files";
-	private static final String UPDATE_URL = "/debt-collection-enforcement/create-fdc-file";
-
-	@Test
-	void testXMLValid() throws Exception {
-		// setup
-		when(fdcMapperUtils.mapFdcEntry(any())).thenCallRealMethod();
-		when(fdcMapperUtils.generateFileXML(any())).thenReturn("<xml>ValidXML</xml>");
-		when(fdcMapperUtils.generateFileName(any())).thenReturn("Test.xml");
-		when(fdcMapperUtils.generateAckXML(any(),any(),any(),any())).thenReturn("<xml>ValidAckXML</xml>");
-		// run
-		boolean successful = fdcService.processDailyFiles();
-		// test
-		verify(fdcMapperUtils).generateFileXML(any());
-		verify(fdcMapperUtils).generateFileName(any());
-		verify(fdcMapperUtils).generateAckXML(any(),any(),any(),any());
-		verify(fdcMapperUtils,times(12)).mapFdcEntry(any());
-		softly.assertThat(successful).isTrue();
-		WireMock.verify(1, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(1, postRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
-
-	@Test
-	void testFdcGlobalUpdateUnsuccessful(){
-		// setup
-		ObjectFactory of = new ObjectFactory();
-		when(fdcMapperUtils.generateFileXML(any())).thenReturn(null);
-		when(fdcMapperUtils.mapFdcEntry(any())).thenReturn(of.createFdcFileFdcListFdc());
-		customStubs.add(stubFor(post(PREPARE_URL).atPriority(1)
-				.willReturn(ok("""
-						{
-						          "successful": false,
-						          "numberOfUpdates": 0
-						        }
-						""").withHeader("Content-Type", "application/json"))));
-		// run
-		boolean successful = fdcService.processDailyFiles();
-
-
-		// test
-		softly.assertThat(successful).isFalse();
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
-
-	@Test
-	void testGetFdcError() {
-		// setup
-		customStubs.add(stubFor(get(GET_URL).atPriority(1)
-				.willReturn(serverError())));
-		// do
-		assertThrows(HttpServerErrorException.class, () -> {
-			fdcService.processDailyFiles();
-		});
-		// test
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(1, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
-
-	@Test
-	void testPrepareFdcError() {
-		// setup
-		customStubs.add(stubFor(post(PREPARE_URL).atPriority(1)
-				.willReturn(serverError())));
-		// do
-		Exception exception = assertThrows(HttpServerErrorException.class, () -> {
-			fdcService.processDailyFiles();
-		});
-		// test
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
-
-	@Test
-	void testGetFdcNoResults() {
-		// setup
-		when(fdcMapperUtils.generateFileXML(any())).thenReturn(null);
-		customStubs.add(stubFor(get(GET_URL).atPriority(1)
-				.willReturn(ok("""
-                        {
-                            "fdcContributions": []
-                        }
-                        """).withHeader("Content-Type", "application/json"))));
-		// do
-		boolean successful = fdcService.processDailyFiles();
-		// test
-		softly.assertThat(successful).isFalse();
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(1, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(0, getRequestedFor(urlEqualTo(UPDATE_URL)));
-		verify(fdcMapperUtils,times(0)).generateFileXML(any());
-	}
-
-	@Test
-	void testAtomicUpdateFailure(){
-		// setup
-		customStubs.add(stubFor(post(UPDATE_URL).atPriority(1)
-				.willReturn(serverError())));
-
-		when(fdcMapperUtils.mapFdcEntry(any())).thenCallRealMethod();
-		when(fdcMapperUtils.generateFileXML(any())).thenReturn("<xml>ValidXML</xml>");
-		when(fdcMapperUtils.generateFileName(any())).thenReturn("Test.xml");
-		when(fdcMapperUtils.generateAckXML(any(),any(),any(),any())).thenReturn("<xml>ValidAckXML</xml>");
-
-		// do
-		Exception exception = assertThrows(HttpServerErrorException.class, () -> {
-			fdcService.processDailyFiles();
-		});
-		// test
-		WireMock.verify(1, postRequestedFor(urlEqualTo(PREPARE_URL)));
-		WireMock.verify(1, getRequestedFor(urlEqualTo(GET_URL)));
-		WireMock.verify(1, postRequestedFor(urlEqualTo(UPDATE_URL)));
-	}
 
 	@Test
 	void testProcessFdcUpdateWhenReturnedTrue() {
 		UpdateLogFdcRequest dataRequest = UpdateLogFdcRequest.builder()
-				.fdcId(911)
+				.fdcId(31774046)
 				.build();
 		String response = fdcService.processFdcUpdate(dataRequest);
 		assertEquals("The request has been processed successfully", response);
