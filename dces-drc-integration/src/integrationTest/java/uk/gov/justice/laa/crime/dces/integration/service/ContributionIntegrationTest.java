b9a3ebf213163b70f5c34b61feabb93c16fd2929
David Conneely <899881+dconneely@users.noreply.github.com>
2024-06-04 12:47:17 +0100

    DCES-333 pre-requisites for first functional tests
    * Modified `build.gradle` to allow Lombok in both `test` and `integrationTest` tests
    * Added simple `TestDataClient` interface for generated client to the DCES test-data services in maat-api
    * Added model (dto) classes for the update-status test-data service
    * Added a (disabled for now) integration test method (will be extended to a full test case in time)

--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/ContributionIntegrationTest.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/ContributionIntegrationTest.java
@@ -4,13 +4,19 @@ import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
+import uk.gov.justice.laa.crime.dces.integration.client.TestDataClient;
+import uk.gov.justice.laa.crime.dces.integration.model.external.ConcorContributionStatus;
+import uk.gov.justice.laa.crime.dces.integration.model.external.UpdateConcorContributionStatusRequest;
 import uk.gov.justice.laa.crime.dces.integration.model.external.UpdateLogContributionRequest;
+
+import java.util.List;
 
 
 @EnabledIf(expression = "#{environment['sentry.environment'] == 'development'}", loadContext = true)
@@ -25,6 +31,9 @@ class ContributionIntegrationTest {
 
 	@Autowired
 	private ContributionService contributionService;
+
+	@Autowired
+	private TestDataClient testDataClient;
 
 	@AfterEach
 	void assertAll(){
@@ -52,6 +61,16 @@ class ContributionIntegrationTest {
 				.build();
 		String response = contributionService.processContributionUpdate(dataRequest);
 		softly.assertThat(response).isEqualTo("The request has been processed successfully");
+	}
+
+	@Test @Disabled("This test currently fails until Cognito auth in maat-api is fixed")
+	void testOneContributionStatusChanges() {
+		UpdateConcorContributionStatusRequest dataRequest = UpdateConcorContributionStatusRequest.builder()
+				.status(ConcorContributionStatus.ACTIVE)
+				.recordCount(1)
+				.build();
+		List<Long> response = testDataClient.updateConcurContributionStatus(dataRequest);
+		softly.assertThat(response).hasSize(1);
 	}
 
 }
