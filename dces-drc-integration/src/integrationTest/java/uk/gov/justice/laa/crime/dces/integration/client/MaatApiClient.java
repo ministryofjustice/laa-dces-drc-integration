9f2a804175add012774aeaa29233144064791302
Dave Edwards <dave.edwards@digital.justice.gov.uk>
2024-07-19 13:51:37 +0100

    Adding fdc Delayed and fdc Fast Track endpoints.

--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/client/MaatApiClient.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/client/MaatApiClient.java
@@ -4,19 +4,51 @@ import io.restassured.response.ValidatableResponse;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.crime.dces.integration.utils.RequestSpecificationBuilder;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static net.serenitybdd.rest.SerenityRest.given;
 
 @Component
 public class MaatApiClient {
 
-    private final String BASE_URL = "debt-collection-enforcement/";
+    private static final String DCES_BASE_URL = "debt-collection-enforcement/";
+    private static final String ASSESSMENT_BASE_URL = "assessment/";
 
     public ValidatableResponse getFdcList(String status) {
         final String CONTRIBUTION_FILES_URI = "fdc-contribution-files";
         return given()
                 .spec(RequestSpecificationBuilder.getMaatAPICrimeApplyReqSpec())
                 .param("status", status)
-                .get(BASE_URL + CONTRIBUTION_FILES_URI)
+                .get(DCES_BASE_URL + CONTRIBUTION_FILES_URI)
+                .then()
+                .log()
+                .all();
+    }
+
+    public ValidatableResponse getFdcFastTrackRepOrderIdList(int delay, LocalDate dateRecieved, int numRecords) {
+        final String REP_ORDERS_URI = "rep-orders";
+        return given()
+                .spec(RequestSpecificationBuilder.getMaatAPICrimeApplyReqSpec())
+                .param("fdcFastTrack", "true")
+                .param("delay", delay)
+                .param("dateReceived", dateRecieved.format(DateTimeFormatter.ISO_DATE))
+                .param("numRecords", numRecords)
+                .get(ASSESSMENT_BASE_URL + REP_ORDERS_URI)
+                .then()
+                .log()
+                .all();
+    }
+
+    public ValidatableResponse getFdcDelayedRepOrderIdList(int delay, LocalDate dateRecieved, int numRecords) {
+        final String REP_ORDERS_URI = "rep-orders";
+        return given()
+                .spec(RequestSpecificationBuilder.getMaatAPICrimeApplyReqSpec())
+                .param("fdcDelayedPickup", "true")
+                .param("delay", delay)
+                .param("dateReceived", dateRecieved.format(DateTimeFormatter.ISO_DATE))
+                .param("numRecords", numRecords)
+                .get(ASSESSMENT_BASE_URL + REP_ORDERS_URI)
                 .then()
                 .log()
                 .all();
--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/NewIntegrationTest.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/service/NewIntegrationTest.java
@@ -1,17 +1,22 @@
 package uk.gov.justice.laa.crime.dces.integration.service;
 
 import io.restassured.response.ValidatableResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.justice.laa.crime.dces.integration.client.MaatApiClient;
+import uk.gov.justice.laa.crime.dces.integration.model.generated.fdc.FdcFile;
 import uk.gov.justice.laa.crime.dces.integration.model.generated.fdc.FdcFile.FdcList.Fdc;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.empty;
 
+@Slf4j
 @SpringBootTest
 class NewIntegrationTest {
 
@@ -29,12 +34,29 @@ class NewIntegrationTest {
         response.assertThat().statusCode(200);
         response.body("fdcContributions", not(empty()));
     }
+    @Test
+    void testFdcFastTrack_ApiClient() {
+
+        LocalDate date = LocalDate.of(2011,12,12);
+        ValidatableResponse response = maatApiClient.getFdcFastTrackRepOrderIdList(5, date,5);
+
+        response.assertThat().statusCode(200);
+        response.body("", not(empty()));
+    }
+    @Test
+    void testFdcDelay_ApiClient() {
+        LocalDate date = LocalDate.of(2011,12,12);
+        ValidatableResponse response = maatApiClient.getFdcDelayedRepOrderIdList(5, date,5);
+
+        response.assertThat().statusCode(200);
+        response.body("", not(empty()));
+    }
 
     @Test
     void testFDCList_Service() {
 
         List<Fdc> fdcList = fdcService.getFdcList();
-        System.out.println(fdcList);
+        log.info(fdcList.stream().map(x->x.getId().toString()).collect(Collectors.joining(",")));
     }
 
 
