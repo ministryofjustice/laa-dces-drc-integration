006665dd4a7866475d7fcdf3fb41487b5dcff4c0
athar.majeed <athar.majeed@digital.justice.gov.uk>
2024-07-29 19:01:07 +0100

    updated config.

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/aspect/CountedAspect.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/aspect/CountedAspect.java
@@ -2,19 +2,18 @@ package uk.gov.justice.laa.crime.dces.integration.aspect;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class CountedAspect {
 
-    private static final Logger logger = LoggerFactory.getLogger(CountedAspect.class);
     private final MeterRegistry registry;
 
     public CountedAspect(MeterRegistry registry) {
@@ -28,13 +27,14 @@ public class CountedAspect {
     @After("countedClasses()")
     public void afterCountedMethod(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().toShortString();
-        Counter counter = Counter.builder(methodName)
+        log.info("Method name is {}", methodName);
+        Counter counter = Counter.builder("allAPIsCalls")
                 .description("Number of calls to " + methodName)
                 .register(registry);
+
         counter.increment();
 
-        logger.info("Method {} was called, current count is {}", methodName, counter.count());
-
+        log.info("Method {} was called, current count is {}", methodName, counter.count());
     }
 
 }
