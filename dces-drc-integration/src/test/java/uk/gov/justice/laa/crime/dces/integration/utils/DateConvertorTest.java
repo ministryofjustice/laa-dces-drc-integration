79d78d92618def708785a4fd23e6a425dddb18d1
athar.majeed <athar.majeed@digital.justice.gov.uk>
2024-10-09 19:20:04 +0100

    Added more test coverage

--- dces-drc-integration/src/test/java/uk/gov/justice/laa/crime/dces/integration/utils/DateConvertorTest.java
+++ dces-drc-integration/src/test/java/uk/gov/justice/laa/crime/dces/integration/utils/DateConvertorTest.java
@@ -2,12 +2,15 @@ package uk.gov.justice.laa.crime.dces.integration.utils;
 
 import org.junit.jupiter.api.Test;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mockStatic;
 
 class DateConvertorTest {
 
@@ -25,4 +28,18 @@ class DateConvertorTest {
         XMLGregorianCalendar xmlGregorianCalendar = DateConvertor.convertToXMLGregorianCalendar(date);
         assertNull(xmlGregorianCalendar);
     }
+
+    @Test
+    void convertToXMLGregorianCalendarWhenThrowsDatatypeConfigurationException() {
+        LocalDate date = LocalDate.of(2023, 10, 1);
+
+        try (var mockedDatatypeFactory = mockStatic(DatatypeFactory.class)) {
+            mockedDatatypeFactory.when(DatatypeFactory::newInstance)
+                    .thenThrow(new DatatypeConfigurationException("Mocked when date parsing exception"));
+
+            XMLGregorianCalendar xmlGregorianCalendar = DateConvertor.convertToXMLGregorianCalendar(date);
+            assertNull(xmlGregorianCalendar);
+        }
+    }
+
 }
