8061fb17be8f567d9283e12eb5ce24e4195ee6bc
David Conneely <899881+dconneely@users.noreply.github.com>
2024-10-17 22:39:55 +0100

    DCES-534 Change incoming APIs to use ProblemDetail report

--- dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/model/FdcAckFromDrc.java
+++ dces-drc-integration/src/main/java/uk/gov/justice/laa/crime/dces/integration/model/FdcAckFromDrc.java
@@ -1,12 +1,18 @@
 package uk.gov.justice.laa.crime.dces.integration.model;
 
+import org.springframework.http.ProblemDetail;
+
 import java.util.Map;
 
 public record FdcAckFromDrc(FdcAckData data, Map<String, String> meta) {
-    public record FdcAckData(int fdcId, Integer maatId, String errorText) {
+    public record FdcAckData(int fdcId, Integer maatId, ProblemDetail report) {
+        public String errorText() {
+            return ProblemDetails.toErrorText(report);
+        }
     }
 
-    public static FdcAckFromDrc of(int fdcId, String errorText) {
-        return new FdcAckFromDrc(new FdcAckData(fdcId, null, errorText), Map.of());
+    public static FdcAckFromDrc of(final int fdcId, final String errorText) {
+        return new FdcAckFromDrc(new FdcAckData(fdcId, null,
+                ProblemDetails.fromErrorText(errorText)), Map.of());
     }
 }
