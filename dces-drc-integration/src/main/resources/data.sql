9b2b54df592c803a7d450018581035d5551b0536
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2024-10-25 12:11:45 +0100

    DCES-480 Flyway (#111)
    * Initial Flyway commit.
    
    * Adding flyway to gradle.
    
    * Adding flyway to gradle.
    
    * Adding flyway to gradle.
    
    * Adding postgresql flyway artifact.
    
    * Updating flywayVersion.

--- dces-drc-integration/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO record_type VALUES ('Contribution'), ('Fdc');
-INSERT INTO event_type (description) VALUES ('FetchedFomMAAT'),('SyncRequestResponseToDrc'),('SyncResponseLoggedToMAAT'),('AsyncRequestResponseFromDrc');
-
