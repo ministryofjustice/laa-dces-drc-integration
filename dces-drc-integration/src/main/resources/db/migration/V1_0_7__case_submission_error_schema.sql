074e200de6f2e6e481d15e4075bcb17bba779d7f
athar.majeed <athar.majeed@justice.gov.uk>
2025-10-27 15:35:03 +0000

    file format

--- dces-drc-integration/src/main/resources/db/migration/V1_0_7__case_submission_error_schema.sql
+++ dces-drc-integration/src/main/resources/db/migration/V1_0_7__case_submission_error_schema.sql
@@ -4,22 +4,12 @@ CREATE SEQUENCE IF NOT EXISTS case_submission_error_gen_seq
 
 CREATE TABLE IF NOT EXISTS case_submission_error
 (
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    maat_id
-    INT,
-    concor_contribution_id
-    INT,
-    fdc_id
-    INT,
-    title
-    VARCHAR
-(
-    255
-),
+    id SERIAL PRIMARY KEY,
+    maat_id INT,
+    concor_contribution_id INT,
+    fdc_id INT,
+    title VARCHAR( 255),
     status INT,
     detail TEXT,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+);
