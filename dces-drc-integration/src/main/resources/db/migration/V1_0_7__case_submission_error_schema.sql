c17244fd8de2096f96a20b6abea1a8c4c3d7c2ab
athar.majeed <athar.majeed@justice.gov.uk>
2025-10-27 13:39:57 +0000

    Add CaseSubmissionError entity, repository, and schema

---
+++ dces-drc-integration/src/main/resources/db/migration/V1_0_7__case_submission_error_schema.sql
@@ -0,0 +1,26 @@
+CREATE SEQUENCE IF NOT EXISTS case_submission_error_gen_seq
+    INCREMENT 1
+    START 1;
+
+CREATE TABLE IF NOT EXISTS case_submission_error
+(
+    id
+    SERIAL
+    PRIMARY
+    KEY,
+    maat_id
+    INT,
+    concor_contribution_id
+    INT,
+    fdc_id
+    INT,
+    title
+    VARCHAR
+(
+    255
+),
+    status INT,
+    detail TEXT,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    );
