1b9ceed8098076cb9d744134292dfc730f9c0d30
athar.majeed <athar.majeed@justice.gov.uk>
2025-10-30 19:45:20 +0000

    Enabled the endpoints + integration test

--- dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/testing/ContributionLoggingIntegrationTest.java
+++ dces-drc-integration/src/integrationTest/java/uk/gov/justice/laa/crime/dces/integration/testing/ContributionLoggingIntegrationTest.java
@@ -5,7 +5,6 @@ import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -86,7 +85,6 @@ class ContributionLoggingIntegrationTest {
      *
      * @see <a href="https://dsdmoj.atlassian.net/browse/DCES-354">DCES-354</a> for test specification.
      */
-    @Disabled("Disabled as the controller has been temporarily removed.")
     @Test
     void givenSomeActiveConcorContributionsAndProcessDailyFilesRan_whenDrcRespondsToAcknowledge_thenContributionsAndFileAreUpdated() {
         // Update at least 3 concor_contribution rows to ACTIVE:
@@ -161,7 +159,6 @@ class ContributionLoggingIntegrationTest {
      *
      * @see <a href="https://dsdmoj.atlassian.net/browse/DCES-355">DCES-355</a> for test specification.
      */
-    @Disabled("Disabled as the controller has been temporarily removed.")
     @Test
     void givenSomeActiveConcorContributionsAndProcessDailyFilesRan_whenDrcRespondsWithError_thenContributionFileIsNotUpdatedButErrorIsCreated() {
         // Update at least 3 concor_contribution rows to ACTIVE:
