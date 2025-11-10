6323dd587b62a4970fd3083e5a68b254b4122e86
athar.majeed <athar.majeed@justice.gov.uk>
2025-11-10 18:10:40 +0000

    Add tests for logging FDC and ConcorContribution errors in EventService

--- dces-drc-integration/src/test/java/uk/gov/justice/laa/crime/dces/integration/datasource/EventServiceTest.java
+++ dces-drc-integration/src/test/java/uk/gov/justice/laa/crime/dces/integration/datasource/EventServiceTest.java
@@ -21,6 +21,8 @@ import uk.gov.justice.laa.crime.dces.integration.datasource.repository.CaseSubmi
 import uk.gov.justice.laa.crime.dces.integration.datasource.repository.CaseSubmissionRepository;
 import uk.gov.justice.laa.crime.dces.integration.datasource.repository.EventTypeRepository;
 import uk.gov.justice.laa.crime.dces.integration.exception.DcesDrcServiceException;
+import uk.gov.justice.laa.crime.dces.integration.model.ConcorContributionAckFromDrc;
+import uk.gov.justice.laa.crime.dces.integration.model.FdcAckFromDrc;
 import uk.gov.justice.laa.crime.dces.integration.model.generated.contributions.CONTRIBUTIONS;
 import uk.gov.justice.laa.crime.dces.integration.model.generated.fdc.FdcFile.FdcList.Fdc;
 
@@ -29,6 +31,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -82,6 +85,121 @@ class EventServiceTest {
         softly.assertThat(result).isEqualTo(input);
         softly.assertAll();
     }
+    @Test
+    void whenLogFdcErrorIsCalled_thenEntitySavedAndReturned() {
+        var ack = mock(FdcAckFromDrc.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+        var pd = mock(org.springframework.http.ProblemDetail.class);
+        when(pd.getTitle()).thenReturn("C Title");
+        when(pd.getDetail()).thenReturn("C Detail");
+        when(pd.getStatus()).thenReturn(500);
+        when(ack.data().report()).thenReturn(pd);
+        when(ack.data().fdcId()).thenReturn(testFdcId);
+        when(ack.data().maatId()).thenReturn(testMaatId);
+        when(ack.data().errorText()).thenReturn("ignored");
+
+        when(caseSubmissionErrorRepository.save(any(CaseSubmissionErrorEntity.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        var result = eventService.logFdcError(ack);
+
+        verify(caseSubmissionErrorRepository).save(caseSubmissionErrorEntityArgumentCaptor.capture());
+        var captured = caseSubmissionErrorEntityArgumentCaptor.getValue();
+
+        softly.assertThat(result).isEqualTo(captured);
+        softly.assertThat(captured.getMaatId()).isEqualTo(testMaatId);
+        softly.assertThat(captured.getFdcId()).isEqualTo(testFdcId);
+        softly.assertThat(captured.getTitle()).isEqualTo("C Title");
+        softly.assertThat(captured.getDetail()).isEqualTo("C Detail");
+        softly.assertThat(captured.getStatus()).isEqualTo(Integer.valueOf(500));
+        softly.assertAll();
+    }
+    @Test
+    void whenLogFdcErrorRepositoryThrows_thenReturnsNull() {
+        var ack = mock(FdcAckFromDrc.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+        var pd = mock(org.springframework.http.ProblemDetail.class);
+        when(pd.getTitle()).thenReturn("C Title");
+        when(pd.getDetail()).thenReturn("C Detail");
+        when(pd.getStatus()).thenReturn(500);
+        when(ack.data().report()).thenReturn(pd);
+        when(ack.data().fdcId()).thenReturn(testFdcId);
+        when(ack.data().maatId()).thenReturn(testMaatId);
+        when(ack.data().errorText()).thenReturn("ignored");
+
+        when(caseSubmissionErrorRepository.save(any(CaseSubmissionErrorEntity.class)))
+                .thenThrow(new RuntimeException("DB down"));
+
+        var result = eventService.logFdcError(ack);
+
+        verify(caseSubmissionErrorRepository).save(caseSubmissionErrorEntityArgumentCaptor.capture());
+        var captured = caseSubmissionErrorEntityArgumentCaptor.getValue();
+
+        softly.assertThat(result).isNull();
+        softly.assertThat(captured.getMaatId()).isEqualTo(testMaatId);
+        softly.assertThat(captured.getFdcId()).isEqualTo(testFdcId);
+        softly.assertThat(captured.getTitle()).isEqualTo("C Title");
+        softly.assertThat(captured.getDetail()).isEqualTo("C Detail");
+        softly.assertThat(captured.getStatus()).isEqualTo(Integer.valueOf(500));
+        softly.assertAll();
+    }
+
+    @Test
+    void whenLogConcorContributionErrorIsCalled_thenEntitySavedAndReturned() {
+        var ack = mock(ConcorContributionAckFromDrc.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+        var pd = mock(org.springframework.http.ProblemDetail.class);
+        when(pd.getTitle()).thenReturn("C Title");
+        when(pd.getDetail()).thenReturn("C Detail");
+        when(pd.getStatus()).thenReturn(500);
+        when(ack.data().report()).thenReturn(pd);
+        when(ack.data().concorContributionId()).thenReturn(testConcorId);
+        when(ack.data().maatId()).thenReturn(testMaatId);
+        when(ack.data().errorText()).thenReturn("ignored");
+
+        when(caseSubmissionErrorRepository.save(any(CaseSubmissionErrorEntity.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        var result = eventService.logConcorContributionError(ack);
+
+        verify(caseSubmissionErrorRepository).save(caseSubmissionErrorEntityArgumentCaptor.capture());
+        var captured = caseSubmissionErrorEntityArgumentCaptor.getValue();
+
+        softly.assertThat(result).isEqualTo(captured);
+        softly.assertThat(captured.getMaatId()).isEqualTo(testMaatId);
+        softly.assertThat(captured.getConcorContributionId()).isEqualTo(testConcorId);
+        softly.assertThat(captured.getTitle()).isEqualTo("C Title");
+        softly.assertThat(captured.getDetail()).isEqualTo("C Detail");
+        softly.assertThat(captured.getStatus()).isEqualTo(Integer.valueOf(500));
+        softly.assertAll();
+    }
+
+    @Test
+    void whenLogConcorContributionErrorRepositoryThrows_thenReturnsNull() {
+        var ack = mock(ConcorContributionAckFromDrc.class, org.mockito.Mockito.RETURNS_DEEP_STUBS);
+        var pd = mock(org.springframework.http.ProblemDetail.class);
+        when(pd.getTitle()).thenReturn("C Title");
+        when(pd.getDetail()).thenReturn("C Detail");
+        when(pd.getStatus()).thenReturn(500);
+        when(ack.data().report()).thenReturn(pd);
+        when(ack.data().concorContributionId()).thenReturn(testConcorId);
+        when(ack.data().maatId()).thenReturn(testMaatId);
+        when(ack.data().errorText()).thenReturn("ignored");
+
+        when(caseSubmissionErrorRepository.save(any(CaseSubmissionErrorEntity.class)))
+                .thenThrow(new RuntimeException("DB down"));
+
+        var result = eventService.logConcorContributionError(ack);
+
+        verify(caseSubmissionErrorRepository).save(caseSubmissionErrorEntityArgumentCaptor.capture());
+        var captured = caseSubmissionErrorEntityArgumentCaptor.getValue();
+
+        softly.assertThat(result).isNull();
+        softly.assertThat(captured.getMaatId()).isEqualTo(testMaatId);
+        softly.assertThat(captured.getConcorContributionId()).isEqualTo(testConcorId);
+        softly.assertThat(captured.getTitle()).isEqualTo("C Title");
+        softly.assertThat(captured.getDetail()).isEqualTo("C Detail");
+        softly.assertThat(captured.getStatus()).isEqualTo(Integer.valueOf(500));
+        softly.assertAll();
+    }
+
 
     @Test
     void whenLogFdcIsCalledWithAllDetails_thenLogEntryIsAsExpected() {
